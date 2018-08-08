c191a151e7c68dd72f2c7a4e7ad8b361a98c4ef5
Vitaly Yurenya <vitaly.yurenya@gmail.com>
2018-08-08 19:49:12 +0300

    Add LF after my name

--- index.js
+++ index.js
@@ -3,15 +3,7 @@ const Koa = require('koa');
 const app = new Koa();
 
 app.use(async (ctx) => {
-  ctx.body = 'Vitaly';
+  ctx.body = 'Vitaly\n';
 });
 
 app.listen(5500);
-
-// commit 1
-// commit 2
-// commit 3
-// commit 4
-
-// some change
-// ыавыа
